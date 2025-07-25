c9964406e94eb350a8e565548dc0394ab7fdb3c6
MayurThakkar-178 <22bce178@nirmauni.ac.in>
2025-07-26 02:02:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 // Start webcam stream
+console.log("Script loaded");
+
+const canvas = document.getElementById('gameCanvas');
+if (!canvas) {
+  console.error("Canvas not found!");
+}
+const ctx = canvas.getContext('2d');
+
+let fruitImg = new Image();
+fruitImg.src = 'assets/watermelon.png';
+fruitImg.onload = () => {
+  console.log("Fruit image loaded");
+  ctx.drawImage(fruitImg, 300, 200, 100, 100);
+};
+
 navigator.mediaDevices.getUserMedia({ video: true })
   .then(function(stream) {
     const video = document.getElementById('webcam');
