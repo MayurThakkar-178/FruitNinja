d7561c3ebab9cf5ce61b5f56f9f12645395c5645
MayurThakkar-178 <22bce178@nirmauni.ac.in>
2025-07-26 01:32:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+body {
+  margin: 0;
+  text-align: center;
+  background-color: #a0e8ff;
+  font-family: 'Comic Sans MS', cursive, sans-serif;
+}
+
+h1 {
+  margin: 10px;
+  color: #fff;
+  background: #2d98da;
+  padding: 10px;
+  border-radius: 10px;
+  width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+#gameCanvas {
+  background: url('assets/background.png') no-repeat center center;
+  background-size: cover;
+  border: 3px solid #fff;
+}
+
+#webcam {
+  display: none;
+}
